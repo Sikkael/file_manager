e72d1bb954ad8af725e5f6f3aaf380cd3243a99b
Michel Ouellet <mouellet@live.ca>
2026-02-09 20:06:45 -0500

    Directory adding functionality

--- fileman/fileman.py
+++ fileman/fileman.py
@@ -100,7 +100,7 @@ class FileManager:
             return CurrentDirectory("", read.error)
         if not dirname in read.file_infos["directories"]:
             read.file_infos["directories"].append(dirname)
-        
+        files_infos = read.file_infos["file_infos"]
         write = self._db_handler.write_file_data(read.file_infos)
         if write.error != SUCCESS:
             return CurrentDirectory("", write.error)
