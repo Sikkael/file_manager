252d3cfefedc0f0807af6008664a39c29a3484fb
Michel Ouellet <mouellet@live.ca>
2026-03-26 12:03:37 -0400

    Starting to creating new file to encapsulate file data

--- fileman/file_stats.py
+++ fileman/file_stats.py
@@ -1,8 +1,9 @@
-# This file contains the default stats for a file manager. It is used to store the stats of the files in the file manager.  
 
 
+
+from dataclasses import asdict
 import sys
-
+from typing import Dict, List
 
 _blank_file_stats = {
     "total_files": 0, 
@@ -24,3 +25,185 @@ _blank_file_stats = {
     }
 
 
+class FilesStats:
+    
+    def __init__(self, total_files: int = 0, total_size: int = 0, biggest_file:str = "", biggest_file_size:int =-sys.maxsize,
+                 smallest_file:str = "", smallest_file_size:int = sys.maxsize,average_file_size:int = 0, oldest_file:str = "", 
+                 oldest_file_date:str = "Mon Feb  8 03:44:42 2100", newest_file:str = "", 
+                 newest_file_date:str = "Mon Feb 3 03:44:42 1902", duplicate_files_count:int = 0, highest_file_duplication_count:int = 0, most_duplicated_file:str = "",
+                 exts:Dict[str,int]={}) -> None:
+        self._total_files = total_files
+        self._total_size = total_size
+        self._biggest_file = biggest_file
+        self._biggest_file_size = biggest_file_size
+        self._smallest_file = smallest_file
+        self._smallest_file_size = smallest_file_size
+        self._average_file_size = average_file_size
+        self._oldest_file = oldest_file
+        self._oldest_file_date = oldest_file_date
+        self._newest_file = newest_file
+        self._newest_file_date = newest_file_date
+        self._duplicate_files_count = duplicate_files_count
+        self._highest_file_duplication_count = highest_file_duplication_count
+        self._most_duplicated_file = most_duplicated_file
+        # list of files extensions found and their numbers
+        self._exts = exts
+        
+        
+    def to_dict(self):
+        # Create a copy of __dict__ and remove internal attributes
+        data_dict = self.__dict__.copy()
+        
+        for key in list(data_dict.keys()):
+            if key.startswith('_'):
+                data_dict[key[1:]] = data_dict.pop(key)
+        
+        return data_dict
+    
+    @property
+    def total_files(self):
+        return self._total_files
+    
+    @total_files.setter
+    def total_files(self, value):
+        if not isinstance(value, int):
+            raise ValueError("total_files must be an integer")
+        val = value if value%2 == 0 else 0
+        self._total_files = val
+        
+    @property
+    def total_size(self):
+        return self._total_size
+    
+    @total_size.setter
+    def total_size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("total_size must be an integer")
+        self._total_size = value
+        
+    @property
+    def biggest_file(self):
+        return self._biggest_file
+    
+    @biggest_file.setter
+    def biggest_file(self, value):
+        if not isinstance(value, str):
+            raise ValueError("biggest_file must be a string")
+        self._biggest_file = value
+    
+    @property
+    def biggest_file_size(self):
+        return self._biggest_file_size
+    
+    @biggest_file_size.setter
+    def biggest_file_size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("biggest_file_size must be an integer")
+        self._biggest_file_size = value
+        
+    @property   
+    def smallest_file(self):
+        return self._smallest_file
+    
+    @smallest_file.setter
+    def smallest_file(self, value):
+        if not isinstance(value, str):
+            raise ValueError("smallest_file must be a string")
+        self._smallest_file = value
+        
+    @property
+    def smallest_file_size(self):
+        return self._smallest_file_size
+    
+    @smallest_file_size.setter
+    def smallest_file_size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("smallest_file_size must be an integer")
+        self._smallest_file_size = value  
+        
+    @property
+    def average_file_size(self):
+        return self._average_file_size
+    
+    @average_file_size.setter
+    def average_file_size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("average_file_size must be an integer")
+        self._average_file_size = value
+        
+    @property
+    def oldest_file(self):
+        return self._oldest_file
+    
+    @oldest_file.setter
+    def oldest_file(self, value):
+        if not isinstance(value, str):
+            raise ValueError("oldest_file must be a string")
+        self._oldest_file = value
+        
+    @property
+    def oldest_file_date(self):
+        return self._oldest_file_date
+    
+    @oldest_file_date.setter
+    def oldest_file_date(self, value):
+        if not isinstance(value, str):
+            raise ValueError("oldest_file_date must be a string")
+        self._oldest_file_date = value
+        
+    @property
+    def newest_file(self):
+        return self._newest_file
+    
+    @newest_file.setter
+    def newest_file(self, value):
+        if not isinstance(value, str):
+            raise ValueError("newest_file must be a string")
+        self._newest_file = value
+        
+    @property
+    def newest_file_date(self):
+        return self._newest_file_date
+    
+    @newest_file_date.setter
+    def newest_file_date(self, value):
+        if not isinstance(value, str):
+            raise ValueError("newest_file_date must be a string")
+        self._newest_file_date = value
+        
+    @property
+    def duplicate_files_count(self):
+        return self._duplicate_files_count
+    
+    @duplicate_files_count.setter
+    def duplicate_files_count(self, value):
+        if not isinstance(value, int):
+            raise ValueError("duplicate_files_count must be an integer")
+        self._duplicate_files_count = value
+        
+    @property
+    def highest_file_duplication_count(self):
+        return self._highest_file_duplication_count
+    
+    @highest_file_duplication_count.setter
+    def highest_file_duplication_count(self, value):
+        if not isinstance(value, int):
+            raise ValueError("highest_file_duplication_count must be an integer")
+        self._highest_file_duplication_count = value
+        
+    @property
+    def most_duplicated_file(self):
+        return self._most_duplicated_file
+
+    @most_duplicated_file.setter
+    def most_duplicated_file(self, value):
+        if not isinstance(value, str):
+            raise ValueError("most_duplicated_file must be a string")
+        self._most_duplicated_file = value
+
+files_stats = FilesStats()
+
+num = int(input("Enter the number: "))
+files_stats.total_files = num
+
+print(files_stats.to_dict())
